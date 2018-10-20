ddc65cf8b0a0d38bf5ac086fae76a69be620c7cb
Won-der-dan <rominator98@gmail.com>
2018-10-20 23:23:47 +0000

    Sort function fixed, values check added

--- quicksort.py
+++ quicksort.py
@@ -1,16 +1,36 @@
 from sys import stderr
 from numpy import arange, ndarray, array, shape, copy
 
+
+
 def quicksort(arr):
     """
     Быстрая сортировка с помощью алгоритма Хоара.
     Принимает range, list, numpy.ndarray.
     """
+
+    def Check_array(arr):
+        """
+        Проверяет типы значений в массиве
+        :param arr:Array
+        :return: True if all correct, else False and print Error
+        """
+        for i in arr:
+            try:
+                float(i)
+            except ValueError:
+                print(ValueError("Values us not correct"))
+                return False
+            except TypeError:
+                print(TypeError("Type is not correct"))
+                return False
+        return True
+
     def swap(A, m, n):
         A[m], A[n] = A[n], A[m]
 
     if isinstance(arr, range):
-        arr = list(arr)
+        arr = array(arr)
     elif isinstance(arr, list):
         arr = array(arr)
     elif not isinstance(arr, ndarray):
@@ -21,39 +41,42 @@ def quicksort(arr):
     if len(shape(arr)) != 1:
         raise Exception("You must provide a one dimensional list or numpy.ndarray")
 
-    def arrange(A, leftIndex, rightIndex):
-        """
-        Переставляет элементы в массиве между
-        двумя индексами так, что все элементы левой
-        половины меньше элементов правой
-        """
-        center = int((leftIndex + rightIndex) / 2)
-        x = A[center]
-        i = leftIndex - 1
-        j = rightIndex + 1
-        while True:
-            i +=1
-            while A[i] < x:
+    if (Check_array(arr)):
+
+        def arrange(A, leftIndex, rightIndex):
+            """
+            Переставляет элементы в массиве между
+            двумя индексами так, что все элементы левой
+            половины меньше элементов правой
+            """
+            center = int((leftIndex + rightIndex) / 2)
+            x = A[center]
+            i = leftIndex - 1
+            j = rightIndex + 1
+            while True:
                 i +=1
+                while A[i] < x:
+                    i +=1
 
-            j -= 1
-            while A[j] > x:
                 j -= 1
+                while A[j] > x:
+                    j -= 1
 
-            if i >= j:
-                return j
+                if i >= j:
+                    return j
 
-            swap(A, i, j)
+                swap(A, i, j)
 
-    def sort(A, leftIndex, rightIndex):
-        "Сортировка Хоара"
-        if leftIndex < rightIndex:
-            p = arrange(A, leftIndex, rightIndex)
-            sort(A, leftIndex, p)
-            sort(A, p+1, rightIndex)
+        def sort(A, leftIndex, rightIndex):
+            "Сортировка Хоара"
+            if leftIndex < rightIndex:
+                p = arrange(A, leftIndex, rightIndex)
+                sort(A, leftIndex, p)
+                sort(A, p+1, rightIndex)
 
-    sort(arr, 0, len(arr)-1)
-    return arr
+        sort(arr, 0, len(arr)-1)
+        return arr
+
 
 maximum = 10000
 a = arange(maximum, 0, -1)
@@ -61,6 +84,10 @@ print(a)
 sortedArray = quicksort(a)
 print(sortedArray)
 
+# a = { "dfsdfsdf", "dfsdfsdfsdf"}
+# sortedArray = quicksort(a)
+# print(sortedArray)
 
 
-print("privet brat")
+
+# print("privet brat")
